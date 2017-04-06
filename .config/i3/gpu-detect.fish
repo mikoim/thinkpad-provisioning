7debe630d26dbaacedd5a4c6f9296fac158816d2
Eshin Kunishima <ek@esh.ink>
2017-04-06 14:57:17 +0900

    add gpu-detect script

---
+++ .config/i3/gpu-detect.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+function is_nvidia
+  lspci | grep -i GeForce > /dev/null
+
+  if test $status -eq 0
+    echo 1
+  else
+    echo 0
+  end
+end
+
+function is_nvidia_render
+  glxinfo | grep -i "OpenGL renderer string" | grep -i GeForce > /dev/null
+
+  if test $status -eq 0
+    echo 1
+  else
+    echo 0
+  end
+end
+
+if test (is_nvidia) -eq 1 -a (is_nvidia_render) -eq 1
+  notify-send -a 'gpu-detect.fish' -i dialog-warning 'OpenGL renderer mismatch!' 'You are using NVIDIA GPU, but OpenGL renderer does not use it.'
+end
