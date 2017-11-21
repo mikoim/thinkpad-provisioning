ab3ddfa1f5ac84f8e7601b267fbaaf79f6f695d3
Eshin Kunishima <ek@esh.ink>
2017-11-22 01:41:25 +0900

    i3wm: don't run any program on thinkpad

--- .config/i3/autorun.sh
+++ .config/i3/autorun.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-xrandr --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --primary --mode 1920x1080 --pos 0x0 --rotate normal &
-feh --bg-scale "$HOME/Pictures/bg.jpg" &
-compton -b --opacity-rule '100:window_type = "dock" && class_g = "i3bar"' &
-dropbox start &
-wine "$HOME/.wine/drive_c/Program Files (x86)/1Password 4/Agile1pAgent.exe" &
-/opt/dunst/bin/dunst &
-"$HOME/.config/i3/gpu-detect.fish" &
-"$HOME/.config/i3/im.sh" &
-
+if [ $(hostname) == "namako" ]; then
+  xrandr --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --primary --mode 1920x1080 --pos 0x0 --rotate normal &
+  feh --bg-max "$HOME/Pictures/bg.jpg" &
+  compton -b --opacity-rule '100:window_type = "dock" && class_g = "i3bar"' &
+  dropbox start &
+  wine "$HOME/.wine/drive_c/Program Files (x86)/1Password 4/Agile1pAgent.exe" &
+  /opt/dunst/bin/dunst &
+  "$HOME/.config/i3/gpu-detect.fish" &
+  "$HOME/.config/i3/im.sh" &
+else
+fi
