ca8215749a253972bde327863da7912fa569b8b6
Eshin Kunishima <ek@esh.ink>
2017-11-24 02:58:16 +0900

    i3wm: fix autorun.sh

--- .config/i3/autorun.sh
+++ .config/i3/autorun.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-if [ $(hostname) == "namako" ]; then
-  xrandr --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --primary --mode 1920x1080 --pos 0x0 --rotate normal &
+if [ "$(hostname -s)" == "namako" ]; then
+  echo "mother ship"
+  # xrandr --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --primary --mode 1920x1080 --pos 0x0 --rotate normal &
   feh --bg-max "$HOME/Pictures/bg.jpg" &
   compton -b --opacity-rule '100:window_type = "dock" && class_g = "i3bar"' &
   dropbox start &
-  wine "$HOME/.wine/drive_c/Program Files (x86)/1Password 4/Agile1pAgent.exe" &
   /opt/dunst/bin/dunst &
   "$HOME/.config/i3/gpu-detect.fish" &
   "$HOME/.config/i3/im.sh" &
 else
+  echo "laptop"
+  feh --bg-max "$HOME/Pictures/bg.png" &
+  dropbox &
+  dunst &
 fi
+
